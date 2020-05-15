d14bc355933fbd5fe5604c282c4c0b423c6c87c7
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2020-05-15 14:13:00 +0300

    init

---
+++ Assigment_10/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assigment_10.Models
+{
+    public partial class Student
+    {
+        public string IndexNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int IdEnrollment { get; set; }
+        public string StudentPassword { get; set; }
+        public string Salt { get; set; }
+
+        public virtual Enrollment IdEnrollmentNavigation { get; set; }
+    }
+}
